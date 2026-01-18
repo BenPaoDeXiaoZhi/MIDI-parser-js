deda6b1acea5a4d48cf9d1f4a21b3d594eff2017
MengFuziComing~ <121215648+BenPaoDeXiaoZhi@users.noreply.github.com>
2026-01-18 11:34:48 +0800

    更新 index.js

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 (()=>{
 
+class PointerView{
+  constructor(buffer){
+    this.view = new DataView(buffer);
+    this.pointer = 0;
+  }
+  readAscii(bytes=1){
+    const ret = "";
+    for(const end=this.pointer+bytes; this.pointer<end; this.pointer++){
+      ret += String.fromCharCode(this.view.getUint8(this.pointer));
+    }
+    return ret;
+  }
+}
+
 const form = document.getElementById("form");
 const input = document.getElementById("input");
 form.addEventListener("submit", (e)=>{
@@ -9,6 +23,9 @@ form.addEventListener("submit", (e)=>{
     alert("pls select file!");
   }
   console.log(file);
+  const arrayBuffer = file.arrayBuffer();
+  const view = new PointerView(arrayBuffer);
+  console.log(view, view.readAscii(4));
 })
 
 })()
